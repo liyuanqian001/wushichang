9b909e6f49525ccc395c0887fbc257d54e781bc4
liyuanqian001 <2239386142@qq.com>
2019-04-19 04:45:28 +0800

    提交HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,4 +8,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
     }
+
+    public void log(){
+        //此处添加了打印日志功能
+        System.out.println("打印日志");
+    }
 }
