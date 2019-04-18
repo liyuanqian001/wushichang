034be360a4374aa9b897a5b2761c97f670ddd10b
liyuanqian001 <2239386142@qq.com>
2019-04-19 04:21:55 +0800

    提交HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Program: pitiaoke
+ * @Date: 2019/4/19
+ * @Author: wushichang
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
